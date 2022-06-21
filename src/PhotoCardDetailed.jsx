cf49c224055132403fe8ed57224132ad272540df
Knopka <knopkaivy@gmail.com>
2022-06-20 18:05:28 -0700

    Update navigation icons in PhotoCardDetailed component - start

--- src/PhotoCardDetailed.jsx
+++ src/PhotoCardDetailed.jsx
@@ -106,29 +106,28 @@ const PhotoCardDetailed = ({
       />
       <div className="PhotoCardDetailed__main">
         <div className="PhotoCardDetailed__imageAndNavigationContainer">
-          <div className="PhotoCardDetailed__navigation PhotoCardDetailed__navigation-left">
-            {ind > 1 && (
-              <BsChevronLeft
-                className="PhotoCardDetailed__navigationIcon icon"
-                onClick={navigateLeft}
-              />
-            )}
-          </div>
           <div className="PhotoCardDetailed__imageContainer">
+            <div className="PhotoCardDetailed__navigation PhotoCardDetailed__navigation-left">
+              {ind > 1 && (
+                <BsChevronLeft
+                  className="PhotoCardDetailed__navigationIcon"
+                  onClick={navigateLeft}
+                />
+              )}
+            </div>
             <img
               src={images[imageUrl]}
               alt="title"
               className="PhotoCardDetailed__image"
             />
-          </div>
-          <div className="PhotoCardDetailed__navigation PhotoCardDetailed__navigation-right">
-            {/* {ind < gallery.photos.length && ( */}
-            {ind < photosCount && (
-              <BsChevronRight
-                className="PhotoCardDetailed__navigationIcon icon"
-                onClick={navigateRight}
-              />
-            )}
+            <div className="PhotoCardDetailed__navigation PhotoCardDetailed__navigation-right">
+              {ind < photosCount && (
+                <BsChevronRight
+                  className="PhotoCardDetailed__navigationIcon"
+                  onClick={navigateRight}
+                />
+              )}
+            </div>
           </div>
         </div>
         <div className="PhotoCardDetailed__descriptionContainer">
