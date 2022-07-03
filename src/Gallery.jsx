0150084baa6c2c547f0b9bf78f070ed3cd2e8c60
Knopka <knopkaivy@gmail.com>
2022-07-03 16:59:56 -0500

    Update useEffect logic for Gallery component

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -13,61 +13,59 @@ const Gallery = ({
   openOverlay,
   closeOverlay,
 }) => {
-  const [gallery, setGallery] = useState({
-    categoryName: '',
-    categoryId: '',
-    categoryDescription: '',
-    categoryCover: '',
-    photos: [],
-  });
+  const [gallery, setGallery] = useState(undefined);
   let params = useParams();
   useEffect(() => {
-    for (let gal of portfolio.categories) {
-      if (gal.categoryId === params.galleryId) {
-        setGallery(gal);
+    if (!gallery || gallery.categoryId !== params.galleryId) {
+      for (let gal of portfolio.categories) {
+        if (gal.categoryId === params.galleryId) {
+          setGallery(gal);
+        }
       }
     }
-
-    console.log('Gallery useEffect');
-  }, [params, portfolio.categories]);
+  }, [params.galleryId, portfolio.categories]);
 
   useEffect(() => {
     window.scrollTo(0, 0);
-    console.log('Gallery useEffect for window');
-  }, [gallery]);
+  }, [params.galleryId]);
 
-  const galleryList = gallery.photos.map((item) => {
-    const url = `${item.photoId.charAt(0).toUpperCase()}${item.photoId
-      .slice(1)
-      .replace('-', '')}md`;
+  if (gallery) {
+    const galleryList = gallery.photos.map((item) => {
+      const url = `${item.photoId.charAt(0).toUpperCase()}${item.photoId
+        .slice(1)
+        .replace('-', '')}md`;
+      return (
+        <PhotoCard
+          imageId={item.photoId}
+          imageURL={url}
+          imageTitle={item.photoTitle}
+          imageDescription={item.photoDescription}
+          isLiked={item.liked}
+          key={item.photoId}
+          openOverlay={openOverlay}
+          toggleLike={toggleLike}
+          galleryName={gallery.categoryName}
+          galleryId={gallery.categoryId}
+        />
+      );
+    });
+
     return (
-      <PhotoCard
-        imageId={item.photoId}
-        imageURL={url}
-        imageTitle={item.photoTitle}
-        imageDescription={item.photoDescription}
-        isLiked={item.liked}
-        key={item.photoId}
-        openOverlay={openOverlay}
-        toggleLike={toggleLike}
-        galleryName={gallery.categoryName}
-        galleryId={gallery.categoryId}
-      />
-    );
-  });
-
-  return (
-    <div className="Gallery">
-      {overlayIsOpen && (
-        <ShareOverlay closeOverlay={closeOverlay} location={overlayInputVal} />
-      )}
-      <div className="Gallery__headingSection">
-        <h2 className="Gallery__header">{gallery.categoryName}</h2>
-        <p className="Gallery__sub">{gallery.categoryDescription}</p>
+      <div className="Gallery">
+        {overlayIsOpen && (
+          <ShareOverlay
+            closeOverlay={closeOverlay}
+            location={overlayInputVal}
+          />
+        )}
+        <div className="Gallery__headingSection">
+          <h2 className="Gallery__header">{gallery.categoryName}</h2>
+          <p className="Gallery__sub">{gallery.categoryDescription}</p>
+        </div>
+        <main className="Gallery__main">{galleryList}</main>
       </div>
-      <main className="Gallery__main">{galleryList}</main>
-    </div>
-  );
+    );
+  }
 };
 
 export default Gallery;
